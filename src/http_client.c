bfa62b2bd339e9e8518563630d53754cb2550f26
Nachintoch <teshanatsch@gmail.com>
2014-02-20 16:55:51 -0600

    El cliente está refinando; todo va bien para empezar el server ^_^

--- src/http_client.c
+++ src/http_client.c
@@ -6,6 +6,9 @@
 #include <unistd.h>
 #include <string.h>
 #define USERAGENT "NACHINTOCH-HTTPCLIENT 1.0"
+#define GET "GET"
+#define HEAD "HEAD"
+#define POST "POST"
 
 /*
  * Cliente de HTTP/1.0
@@ -20,7 +23,8 @@
 //prototipos de funciones
 int create_socket();
 char *get_ip(char *);
-char *build_query(char *host, char *page);
+char *build_query(char *, char *, char *, char *);
+void die();
 
 /*
  * Ejecuta el cliente de HTTP haciendo un request a la página dada como
@@ -28,23 +32,49 @@ char *build_query(char *host, char *page);
  */
 int main(int argc, char *argv[])
 {
-	if(argc == 1)
+	if(argc < 3)
 	{
-		printf("Invoque el programa pasando como argumentos el host deseado ");
-		printf("y la p\u00E1gina que desea consultar del mismo.\n");
-		printf("Por ejemplo: httpclient web.fciencias.unam.mx /acceder\n");
-		printf("Puede omitir la p\u00E1gina.\n");
+		printf("Invoque el programa pasando como argumentos el m\u00E9todo, ");
+		printf("host deseado, la p\u00E1gina que desea consultar del ");
+		printf("mismo y un posible query para pasar por GET.\nPor ejemplo: ");
+		printf("client GET www.google.com / search=nachintoch\nPuede omitir ");
+		printf("la p\u00E1gina y el query.\n");
 		exit(EXIT_FAILURE);
 	}//comprueba que se le hayan dado argumentos de consola
+	if(strcmp(argv[1], GET) != 0 && strcmp(argv[1], HEAD) != 0 &&
+		strcmp(argv[1], POST) != 0)
+	{
+		printf("\"%s\" no es un m\u00E9todo de HTTP/1.0 reconocido\n",
+			argv[1]);
+		exit(EXIT_FAILURE);
+	}//comprueba que se le haya dado un método de HTTP/1.0 válido
+	char *getq;
+	if(argc > 4 && strcmp(argv[1], GET) != 0)
+	{
+		printf("Un query s\u00F3lo deber\u00EDa usarse con el m\u00E9todo ");
+		printf("GET");
+		exit(EXIT_FAILURE);
+	}
+	else if(argc > 4)
+	{
+		getq = malloc(sizeof(char *) *(strlen(argv[4]) +1));
+		memset(getq, 0, strlen(argv[4]) +1);
+		strcat(getq, "?");
+		strcat(getq, argv[4]);
+	}
+	else
+	{
+		getq = "";
+	}//si se pasó un méotodo que no es get con un query
 	struct sockaddr_in *remote;
 	int sock, n;
 	char *ip, *get;
 	char buffer[BUFSIZ +1];
-	char *host = argv[1];
+	char *host = argv[2];
 	char *page;
-	if(argc > 2)
+	if(argc > 3)
 	{
-		page = argv[2];
+		page = argv[3];
 	}
 	else
 	{
@@ -71,7 +101,7 @@ int main(int argc, char *argv[])
 		perror("Error al conectar");
 		exit(EXIT_FAILURE);
 	}//conecta
-	get = build_query(host, page);
+	get = build_query(argv[1], host, page, getq);
 	int sent = 0;
 	while(sent < strlen(get))
 	{
@@ -86,27 +116,55 @@ int main(int argc, char *argv[])
 	memset(buffer, 0, sizeof(buffer));
 	unsigned int data = 0;
 	char *content;
+	char *head;
+	unsigned short int i;
 	while((n = recv(sock, buffer, BUFSIZ, 0)) > 0)
 	{
-		if(data == 0)
+		content = buffer;
+		if(!data)
 		{
-			content = strstr(buffer, "\r\n\r\n");
-			if(content != NULL)
+			head = malloc(sizeof(char *) *strlen(buffer));
+			for(i = 0; buffer[i] != '\r' || buffer[i +1] != '\n' ||
+				buffer[i +2] != '\r' || buffer[i +3] != '\n'; i++)
 			{
-				data = 1;
+				head[i] = buffer[i];
+			}//copia el encabezado del flujo
+			content = strstr(buffer, "\r\n\r\n");
+			if(content)
+			{
 				content += 4;
-			}//indicamos que se ha obtenido el inicio de los datos de entrada
-		}
-		else
-		{
-			content = buffer;
-		}//si se ha obtenido el resto de la página
-		if(data)
-		{
-			printf(content);
-		}//muestra el documento obtenido
+			}//si se encontró la subcadena
+			data = 1;
+		}//si no se ha recuperado el encabezado
+		fprintf(stdout, content);
 		memset(buffer, 0, n);
 	}//recupera los datos recibidos por el server
+	if(head[0] != 'H' || head[1] != 'T' || head[2] != 'T' || head[3] != 'P')
+	{
+		printf("No se reconoce el protoclo de respuesta\n");
+		die();
+	} if(head[4] != '/')
+	{
+		printf("La respuesta HTTP cotiene una expresi\u00F3n mal formada\n");
+		die();
+	} if((head[5] -48 > 1) && (head[6] != '.') && (head[7] -48 != 0
+		|| head[7] -48 != 9))
+	{
+		printf("No se reconoce la versi\u00F3n de HTTP de respuesta\n");
+		die();
+	} switch(head[9]) {
+	case '1' :
+		break;
+	case '2' :
+		break;
+	case '3' :
+		break;
+	case '4' :
+		break;
+	case '5' :
+		break;
+	default :
+	}
 	if(n < 0)
 	{
 		perror("Error al recuperar los datos");
@@ -150,18 +208,28 @@ char *get_ip(char *host)
 }//get_ip
 
 //construye una consulta al server
-char *build_query(char *host, char *page)
+char *build_query(char *method, char *host, char *page, char *getquery)
 {
 	char *query;
 	char *getpage = page;
-	char *tpl = "GET /%s HTTP/1.0\r\nHost: %s\r\nUser-Agent: %s\r\n\r\n";
+	char *tpl = "%s /%s%s HTTP/1.0\r\nHost: %s\r\nUser-Agent: %s\r\n\r\n";
 	if(getpage[0] == '/')
 	{
 		getpage = getpage +1;
 	}//prepara el encabezado
-	query = (char *) malloc(strlen(host) +strlen(getpage) +strlen(USERAGENT)
-		+ strlen(tpl) -5);
-	sprintf(query, tpl, getpage, host, USERAGENT);
+	query = (char *) malloc(strlen(method) +strlen(host) +strlen(getpage)
+		+strlen(getquery) +strlen(USERAGENT) + strlen(tpl) -6);
+	sprintf(query, tpl, method, getpage, getquery, host, USERAGENT);
 	return query;
 }//build_query
+
+//termina el programa en stado abnormal
+void die()
+{
+	free(get);
+	free(remote);
+	free(ip);
+	close(sock);
+	exit(EXIT_FAILURE);
+}//die
 
